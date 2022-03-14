66f633483004c887043a25d25ee076dbb78c4e0d
Priyanshu Mohanty <prince91001@gmail.com>
2022-03-14 17:15:17 +0530

    Started Trees!

---
+++ Trees/CreatingTrees.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Node
+{
+    Node left, right;
+    int data;
+
+    public Node(int data)
+    {
+        this.data=data;
+      
+    }
+}
+public class CreatingTrees {
+    static Node createTree()
+    {
+        Scanner in = new Scanner(System.in);
+        Node root = null;
+
+        System.out.println("ENTER DATA ==> ");
+        
+        int d = in.nextInt();
+       
+
+        if (d == -1)
+        return null;
+
+        root = new Node(d);
+        
+        System.out.println("ENTER LEFT FOR : "+d);
+        root.left = createTree();
+
+        System.out.println("ENTER RIGHT FOR : "+d);
+        root.right = createTree();
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        createTree();
+    }
+}
